65145c9debcfb2031c1156057ba12910fac7e597
Morgan <morgantheplant@gmail.com>
2014-05-24 12:57:12 -0700

    Create WordCount.js
    Have the function WordCount(str) take the str string parameter being passed and return the number of words the string contains (ie. "Never eat shredded wheat" would return 4). Words will be separated by single spaces. 
    
    Use the Parameter Testing feature in the box below to test your code with different arguments.

---
+++ WordCount.js
@@ -0,0 +1,12 @@
+function WordCount(str) { 
+ var myArray = str.split(" ");
+  
+  str = myArray.length;
+  // code goes here  
+  return str; 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+WordCount(readline());       
