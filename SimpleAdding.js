32f774a1062651b010e357b3ea5c1acadc7dd492
Morgan <morgantheplant@gmail.com>
2014-05-24 12:49:35 -0700

    Create SimpleAdding.js
    Have the function SimpleAdding(num) add up all the numbers from 1 to num. For the test cases, the parameter num will be any number from 1 to 1000. 
    
    Use the Parameter Testing feature in the box below to test your code with different arguments.

---
+++ SimpleAdding.js
@@ -0,0 +1,14 @@
+function SimpleAdding(num) { 
+ var counter = 0;
+  for (var i = 1 ; i <=num; i++){
+    counter += i;
+  }
+  num = counter
+  // code goes here  
+  return num; 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+SimpleAdding(readline());           
