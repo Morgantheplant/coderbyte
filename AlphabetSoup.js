85718caf03a9e6e0bb9595acb3519f8c5aa91c5c
Morgan <morgantheplant@gmail.com>
2014-05-24 12:54:55 -0700

    Create AlphabetSoup.js
    Have the function AlphabetSoup(str) take the str string parameter being passed and return the string with the letters in alphabetical order (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not be included in the string. 
    
    Use the Parameter Testing feature in the box below to test your code with different arguments

---
+++ AlphabetSoup.js
@@ -0,0 +1,11 @@
+function AlphabetSoup(str) { 
+var sorted = str.split("").sort().join("");
+  // code goes here  
+  str = sorted
+  return str; 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+AlphabetSoup(readline());           
