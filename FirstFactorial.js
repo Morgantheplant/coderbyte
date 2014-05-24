3cc5e0a145e7d97bdf66ac0ab90ab8d6367d55a1
Morgan <morgantheplant@gmail.com>
2014-05-24 12:47:08 -0700

    Create FirstFactorial.js
    vHave the function FirstFactorial(num) take the num parameter being passed and return the factorial of it (ie. if num = 4, return (4 * 3 * 2 * 1)). For the test cases, the range will be between 1 and 18. 
    
    Use the Parameter Testing feature in the box below to test your code with different arguments

---
+++ FirstFactorial.js
@@ -0,0 +1,17 @@
+function FirstFactorial(num) { 
+var y = 1
+  for (var i = 1; i <= num; i++){
+  
+    y = y*i
+  
+  }
+  
+  num = y
+  // code goes here  
+  return num; 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+FirstFactorial(readline());           
